778fcca1c4e748b5f6569aa57436c939c0bb5fb9
soaibsafi <soaib.safi@gmail.com>
2023-10-11 11:15:02 +0200

    Clean for the assignment

---
+++ src/main/java/de/buw/fm4se/satsolving/task/Tasks.java
@@ -0,0 +1,33 @@
+package de.buw.fm4se.satsolving.task;
+
+public class Tasks {
+
+  public static String task1_a = "";
+  public static String task1_b = "";
+  
+  public static String task2_a = "";
+  public static String task2_b = "";
+  
+  public static String task3_sentence_1 = "";
+  public static String task3_sentence_2 = "";
+  public static String task3_sentence_3 = "";
+  public static String task3_sentence_4 = "";
+
+  public static String task3_validity = "";
+  
+  
+  public static String task4_rule_1 = "";
+  public static String task4_rule_2 = "";
+  public static String task4_rule_3 = "";
+
+  public static String task4_constraint_1 = "";
+  public static String task4_constraint_2 = "";
+  
+  public static String task4_validity = "";
+
+
+  
+
+
+  
+}
