f1ac4b82f0f7bee3bc2d63455e34f85917f6a9c3
soaibsafi <soaib.safi@gmail.com>
2024-10-21 16:56:21 +0200

    🧪Added additional test class

---
+++ src/test/java/de/buw/fm4se/satsolving/tests/AdditionalTest.java
@@ -0,0 +1,23 @@
+package de.buw.fm4se.satsolving.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+import de.buw.fm4se.satsolving.exec.LimbooleExecutor;
+import de.buw.fm4se.satsolving.task.Tasks;
+import de.buw.fm4se.satsolving.utils.CountVariablesAndOperators;
+import de.buw.fm4se.satsolving.utils.FmPlay;
+
+/*
+  * This is an additional test class. You can write your own test cases here. We will check this manually at the end of the deadline.
+*/
+
+public class AdditionalTest {
+
+  @Test
+  void testAdditional() throws IOException, InterruptedException {
+    // Write your test here
+  }
+
+}
